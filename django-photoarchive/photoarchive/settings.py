97e9cbc19730ae181850346da76805e5c59c2d3a
JonnyTheCat <stels80137@gmail.com>
2022-03-13 14:51:20 +0700

    Hide SECRET_KEY

--- django-photoarchive/photoarchive/settings.py
+++ django-photoarchive/photoarchive/settings.py
@@ -1,4 +1,9 @@
 from pathlib import Path
+from os import environ
+
+env = environ.Env(
+    DEBUG = (bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9$9wp-2i%4r6yqb$a5h+g4ue7wp44ca==u$vt@nz%(@6ujxrau'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
