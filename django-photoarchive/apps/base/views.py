43d25795ea5f8f33c89e87e126c175457df0592f
JonnyTheCat <stels80137@gmail.com>
2022-03-25 22:22:42 +0700

    Small changes

--- django-photoarchive/apps/base/views.py
+++ django-photoarchive/apps/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'base/home.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'base/about.html')
