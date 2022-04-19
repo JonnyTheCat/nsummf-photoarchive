5ad1c508e347e9f80c95e0e3fcca9f00ecc57305
JonnyTheCat <stels80137@gmail.com>
2022-04-19 19:08:27 +0700

    Minor changes
    Fast fix of recent commit

--- django-photoarchive/apps/gallery/admin.py
+++ django-photoarchive/apps/gallery/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Photo, Tag, Person, Profile
+from .models import Photo, Tag, Person
 
 # Register your models here.
 
 admin.site.register(Photo)
 admin.site.register(Tag)
 admin.site.register(Person)
-admin.site.register(Profile)
