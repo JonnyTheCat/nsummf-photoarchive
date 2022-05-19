0b94609af0a058f79a9f09c4c2bbd0baea9d6f9c
Gleb Kazakov <gleb.kazakov5832@gmail.com>
2022-05-19 13:09:08 +0700

    Revert "Merge branch 'models_change' of https://github.com/JonnyTheCat/nsummf-photoarchive into models_change"
    This reverts commit 370281c202ee2adcbf8aaae04c716db5da793c06, reversing
    changes made to 6888478564272291667507cc5b5b83dceb87ab1e.

--- django-photoarchive/apps/gallery/admin.py
+++ django-photoarchive/apps/gallery/admin.py
@@ -2,32 +2,9 @@ from django.contrib import admin
 from .models import Photo, Tag, Person, Title, Job
 
 # Register your models here.
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {
-            'fields': ('tag_name', 'tag_type')
-            }),
-        ('Информация для профиля:', {
-            'classes': ('collapse',),
-            'fields': ('has_profile', 'description', 'title_photo')
-            })
-    ]
 
-@admin.register(Person)
-class PersonAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {'fields': ('name',)}),
-        ('Информация для профиля:', {'classes': ('collapse',), 'fields': ('has_profile', 'date_of_birth', 'date_of_death', 'description', 'title_photo')})
-    ]
-
-
-@admin.register(Photo)
-class PhotoAdmin(admin.ModelAdmin):
-    readonly_fields = ('id',)
-    fieldsets = [
-        ('Основная информация', {'classes': ('collapse',), 'fields': ('author', 'description', 'day_of_capture', 'month_of_capture', 'year_of_capture')}),
-        ('Теги и люди на фото', {'classes': ('collapse',), 'fields': ('people', 'tags')}),
-    ]
-    list_display = ("description", "year_of_capture", "id")
-    list_filter = ("year_of_capture", "tags", "people")
+admin.site.register(Photo)
+admin.site.register(Tag)
+admin.site.register(Person)
+admin.site.register(Title)
+admin.site.register(Job)
